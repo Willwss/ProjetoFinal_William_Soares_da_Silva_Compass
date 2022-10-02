e67c3870cf9bf2c0fa3c02e6c97cf71f72090ec9
William Soares da Silva <will_silva_2011@hotmail.com>
2022-10-02 17:19:35 -0300

    V1.0

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,3 +1,16 @@
 Before do
     @app = App.new
   end
+
+
+
+  #NOVA FEATURE ADCIONADA
+
+  After do |scenario|
+
+     #if scenario.failed?
+        screenshot =  Capybara.page.save_screenshot("screenshots/sc.png")
+        attach(screenshot, "image/png")
+     #end
+
+end
